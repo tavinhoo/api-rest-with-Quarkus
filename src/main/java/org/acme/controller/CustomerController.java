cdf1170c8ca7fc39019e80fcd592ca58eec80342
Luiz Otávio <luizotaviomtbs@gmail.com>
2024-05-21 21:58:34 -0300

    Update Customer method(controller + service)

--- src/main/java/org/acme/controller/CustomerController.java
+++ src/main/java/org/acme/controller/CustomerController.java
@@ -2,10 +2,8 @@ package org.acme.controller;
 
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.PUT;
-import jakarta.ws.rs.Path;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Response;
 import org.acme.model.Customer;
 import org.acme.service.CustomerService;
 
@@ -36,6 +34,20 @@ public class CustomerController {
             customerService.addCustomer(customer);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Response updateCustomer(@PathParam("id") Long id, Customer customer) {
+        try {
+            customerService.updateCustomer(id, customer);
+            return Response.noContent().build(); // 204 No Content
+        } catch (Exception e) {
+            if (e.getMessage().equals("Customer not found!")) {
+                return Response.status(Response.Status.NOT_FOUND).entity(e.getMessage()).build(); // 404 Not Found
+            }
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build(); // 500 Internal Server Error
         }
     }
 }
--- src/main/java/org/acme/service/CustomerService.java
+++ src/main/java/org/acme/service/CustomerService.java
@@ -6,6 +6,7 @@ import org.acme.model.Customer;
 import org.acme.repository.CustomerRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @ApplicationScoped
 public class CustomerService {
@@ -17,10 +18,26 @@ public class CustomerService {
         return customerRepository.findAll().list();
     }
 
+    public Optional<Customer> findCustomerById(Long id) {
+        Optional<Customer> customer = Optional.of(customerRepository.findById(id));
+        return customer;
+    }
+
     public void addCustomer(Customer customer) {
         customerRepository.persist(customer);
     }
 
+    public void updateCustomer(Long id, Customer customer) throws Exception {
+        Optional<Customer> customer0 = customerRepository.findByIdOptional(id);
+        if(customer0.isEmpty()) {
+            throw new Exception("Customer not found!");
+        }
 
-
+        Customer customer1 = customer0.get();
+        customer1.setName(customer.getName());
+        customer1.setLastName(customer.getLastName());
+        customer1.setAge(customer.getAge());
+        customer1.setEmail(customer.getEmail());
+        customerRepository.persist(customer1);
+    }
 }
